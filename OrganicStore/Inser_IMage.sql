278b09716edb41b410b9b1082ef513ac9a5dcb39
Kshitij Darwhekar <kshitijdarwhekar@gmail.com>
2024-01-16 00:43:41 +0530

    First Commit
    Fir

---
+++ OrganicStore/Inser_IMage.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE Test(
+ product_id int,
+ pic image,
+)
+
+INSERT INTO TEST(pic) SELECT BulkColumn FROM Openrowset(Bulk 'C:\Users\KshitijDarwhekar\Pictures\Orange.jpg' , Single_blob  ) as img ;
